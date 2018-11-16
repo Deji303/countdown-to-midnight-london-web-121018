d817b397d18321c15767e08789591074c7b30b19
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-16 18:09:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,9 @@
 #write your code here
 
-def countdown (num)
-  counter = 0 
-  while counter < num 
-    puts 
+def countdown (number)
+  
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+  end 
 end
